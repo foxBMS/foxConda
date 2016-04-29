4441db71db33eaabe64f56ba3f5b221d9f508e3b
Tim Fuehner <tim.fuehner@iisb.fraunhofer.de>
2016-04-29 15:58:25 +0200

    - updated to 1.3.2.1, add patch

--- recipes/menuinst/bld.bat
+++ recipes/menuinst/bld.bat
@@ -1,16 +1,2 @@
-::make configure
-::configure --prefix=%PREFIX%
-::make all
-::make install
-
-::cd %PREFIX%
-::del lib lib64
-::del share/man
-::strip bin\* || echo
-::strip libexec\git-core\* || echo
-
-
-PortableGit-2.5.0-64-bit.7z.exe
-copy bld.bat %PREFIX%
-cd PortableGit
-xcopy * %PREFIX% /E /C
+"%PYTHON%" setup.py install
+if errorlevel 1 exit 1
